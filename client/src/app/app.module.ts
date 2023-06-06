9ce1e1d9cad11d723c81190d5c98bf206189f26d
Max Ivanov <axioma162@gmail.com>
2023-06-06 20:12:17 +0300

    add token interseptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { TokenInterceptor } from './shared/token.interseptor';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,9 @@ import { RegisterPageComponent } from './components/register-page/register-page.
     FormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, multi: true, useClass: TokenInterceptor },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
