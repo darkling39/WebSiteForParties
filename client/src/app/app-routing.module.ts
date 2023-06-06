3b15fbab0b4e7814bf7a1905561026b672709afc
Max Ivanov <axioma162@gmail.com>
2023-06-06 19:52:03 +0300

    auth guard done

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -15,7 +16,12 @@ const routes: Routes = [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
     ],
   },
-  { path: '', component: SiteLayoutComponent, children: [] },
+  {
+    path: '',
+    component: SiteLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [],
+  },
 ];
 
 @NgModule({
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
 import { IUser } from '../shared/interfaces';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+  private token = '';
 
-  login(user: IUser): Observable<{ roken: string }> {
-    return this.http.post<{ roken: string }>('/api/auth/login', user);
+  login(user: IUser): Observable<{ token: string }> {
+    return this.http.post<{ token: string }>('/api/auth/login', user).pipe(
+      tap(({ token }) => {
+        localStorage.setItem('auth-token', token);
+        this.setToken(token);
+      })
+    );
   }
-  register() {}
+  setToken(token: string) {
+    this.token = token;
+  }
+  getToken(): string {
+    return this.token;
+  }
+  isAuthenticated(): boolean {
+    return !!this.token;
+  }
+  logOut() {
+    this.setToken('');
+    localStorage.clear();
+  }
+  register(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>('/api/auth/register', user);
+  }
 }
